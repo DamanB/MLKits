a22da343371c1c5f5ced600aee05798644b773fd
DamanB <damanbharaj@gmail.com>
2024-01-09 22:32:03 -0500

    Added standardization

--- knn-tf/index.js
+++ knn-tf/index.js
@@ -2,6 +2,28 @@ require('@tensorflow/tfjs-node');
 const tf = require('@tensorflow/tfjs');
 const loadCSV = require('./load-csv');
 
+/**
+ * k-nearest algorithm. 
+ */
+function knn(features, labels, predictionPoint, k) {
+    const { mean, variance } = tf.moments(features, 0);
+    const scaledPredication = predictionPoint.sub(mean).div(variance.pow(0.5));
+
+    return features
+        .sub(mean) // standardize.
+        .div(variance.pow(0.5))
+        .sub(scaledPredication) // Knn distance calc.
+        .pow(2)
+        .sum(1)
+        .pow(0.5)
+        .expandDims(1) // Knn other steps.
+        .concat(labels, 1)
+        .unstack()
+        .sort((a,b) => a.get(0) > b.get(0) ? 1 : -1)
+        .slice(0, k)
+        .reduce((acc, pair) => acc + pair.get(1), 0) / k;
+}
+
 let { 
     features, 
     labels, 
@@ -14,7 +36,11 @@ let {
     labelColumns: ['price'],
 });
 
-console.log(    
-    testFeatures, 
-    testLabels 
-);
+features = tf.tensor(features);
+labels = tf.tensor(labels);
+
+testFeatures.forEach((testPoint, i) => {
+    const result = knn(features, labels, tf.tensor(testPoint), 10);
+    const err = (testLabels[i][0] - result) / testLabels[i][0];
+    console.log('Error', err * 100)
+})
